14ef1ff53983e0de6fa09c70553ab7ffe5f935a7
zwl <zwlong6@163.com>
2024-08-02 23:38:52 +0800

    修改顶部导航栏

--- themes/gitbook/components/MenuItemDrop.js
+++ themes/gitbook/components/MenuItemDrop.js
@@ -37,7 +37,7 @@ export const MenuItemDrop = ({ link }) => {
       {!hasSubMenu && (
         <div
           className={
-            'px-1 h-full whitespace-nowrap duration-300 text-sm justify-between dark:text-gray-300 cursor-pointer flex flex-nowrap items-center ' +
+            'px-2 h-full whitespace-nowrap duration-300 text-sm justify-between dark:text-gray-300 cursor-pointer flex flex-nowrap items-center ' +
             (selected
               ? 'bg-blue-600 text-white hover:text-white rounded-full border-l-2 border-r-2'
               : 'hover:text-blue-600')

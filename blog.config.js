641d0b50341d46657bb381f0fcdcdb1c6c32e46f
zwl <zwlong6@163.com>
2024-08-02 09:31:42 +0800

    换个字体

--- blog.config.js
+++ blog.config.js
@@ -69,8 +69,8 @@ const BLOG = {
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
     // '"LXGW WenKai"',
-    // '"PingFang SC"',
-    // '-apple-system',
+    '"PingFang SC"',
+    '-apple-system',
     'BlinkMacSystemFont',
     '"Hiragino Sans GB"',
     '"Microsoft YaHei"',

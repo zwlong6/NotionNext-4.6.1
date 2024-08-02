f8e59f25073b1dbdf06cacc9fb7ea95c2261e496
zwl <zwlong6@163.com>
2024-08-02 20:42:55 +0800

    调整logo

--- themes/gitbook/components/MenuItemDrop.js
+++ themes/gitbook/components/MenuItemDrop.js
@@ -28,7 +28,7 @@ export const MenuItemDrop = ({ link }) => {
             {link?.icon && <i className={link?.icon} />} {link?.name}
             {hasSubMenu && (
               <i
-                className={`px-2 fas fa-chevron-down duration-500 transition-all ${show ? ' rotate-180' : ''}`}></i>
+                className={`px-1 fas fa-chevron-down duration-500 transition-all ${show ? ' rotate-180' : ''}`}></i>
             )}
           </div>
         </div>
@@ -37,7 +37,7 @@ export const MenuItemDrop = ({ link }) => {
       {!hasSubMenu && (
         <div
           className={
-            'px-2 h-full whitespace-nowrap duration-300 text-sm justify-between dark:text-gray-300 cursor-pointer flex flex-nowrap items-center ' +
+            'px-1 h-full whitespace-nowrap duration-300 text-sm justify-between dark:text-gray-300 cursor-pointer flex flex-nowrap items-center ' +
             (selected
               ? 'bg-green-600 text-white hover:text-white'
               : 'hover:text-green-600')

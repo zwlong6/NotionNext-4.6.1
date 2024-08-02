0c04847f405185548d7f3633f808593d8690dd51
zwl <zwlong6@163.com>
2024-08-02 18:40:28 +0800

    修改字体

--- blog.config.js
+++ blog.config.js
@@ -68,7 +68,7 @@ const BLOG = {
   ],
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
-    '"LXGW WenKai"',
+    // '"LXGW WenKai"',
     '"PingFang SC"',
     '-apple-system',
     'BlinkMacSystemFont',

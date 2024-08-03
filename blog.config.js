24846297bdcfb04b9dbe2aacbc261f00abc2749d
zwl <zwlong6@163.com>
2024-08-03 10:45:31 +0800

    自定义代码主题

--- blog.config.js
+++ blog.config.js
@@ -176,7 +176,7 @@ const BLOG = {
   PRISM_THEME_LIGHT_PATH:
     // prism.css 
     process.env.NEXT_PUBLIC_PRISM_THEME_LIGHT_PATH ||
-    'https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism-coy.css', // 浅色模式主题
+    'https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism-dark.css', // 浅色模式主题
   PRISM_THEME_DARK_PATH:
     process.env.NEXT_PUBLIC_PRISM_THEME_DARK_PATH ||
     'https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism-okaidia.min.css', // 深色模式主题
